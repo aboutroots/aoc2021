922a70948684d4163ed8272e5c5aaf9fba1217b4
aboutroots <popiel.mw@gmail.com>
2021-12-04 22:33:17 +0000

    day3 part 2

--- day3.dart
+++ day3.dart
@@ -1,25 +1,40 @@
 import './utils.dart';
+import 'package:tuple/tuple.dart';
 
-int computeSimilarity(List input, List target) {
-  int score = 0;
-  for (int i = 0; i < target.length; i++) {
-    if (target[i] != input[i]) {
-      break;
-    }
-    score += 1;
+String filterByIndex(
+    List<String> numbers, int index, String atu, String operation) {
+  if (numbers.length == 1) {
+    return numbers[0];
   }
-  return score;
+  int targetCounter = numbers.fold(0, (int acc, String n) {
+    return acc + int.parse(n[index]);
+  });
+
+  String targetChar = targetCounter < numbers.length / 2 ? '1' : '0';
+  if (operation == 'gt') {
+    targetChar = targetCounter > numbers.length / 2 ? '1' : '0';
+  }
+
+  if (targetCounter == numbers.length / 2) {
+    targetChar = atu;
+  }
+
+  return filterByIndex(
+      numbers.where((String n) => n[index] == targetChar).toList(),
+      index + 1,
+      atu,
+      operation);
 }
 
 void solve(List<String> lines) {
   List<int> acc = List.filled(lines[0].length, 0);
-  List<List<int>> numbers = [];
+  List<String> numbers = [];
 
   // Sum "ones" for every column, also save parsed lines to separate array
   lines.fold(acc, (List<int> accumulator, String line) {
     List<int> lineValues =
         line.trim().split('').map((String char) => int.parse(char)).toList();
-    numbers.add(lineValues);
+    numbers.add(lineValues.join(''));
 
     lineValues.asMap().forEach((index, value) {
       accumulator[index] += value;
@@ -28,20 +43,18 @@ void solve(List<String> lines) {
   });
 
   // first part
-  List<int> gammaArr = acc.map((e) => e > lines.length / 2 ? 1 : 0).toList();
-  List<int> epsilonArr = acc.map((e) => e < lines.length / 2 ? 1 : 0).toList();
+  String gammaStr =
+      acc.map((e) => e > lines.length / 2 ? 1 : 0).toList().join('');
+  String epsilonStr =
+      acc.map((e) => e < lines.length / 2 ? 1 : 0).toList().join('');
 
-  print(int.parse(gammaArr.join(''), radix: 2) *
-      int.parse(epsilonArr.join(''), radix: 2));
+  print(int.parse(gammaStr, radix: 2) * int.parse(epsilonStr, radix: 2));
 
   // second part
-  numbers.sort((a, b) =>
-      computeSimilarity(a, gammaArr) - computeSimilarity(b, gammaArr));
-  List<int> o2gen = numbers[0];
-  numbers.sort((a, b) =>
-      computeSimilarity(a, epsilonArr) - computeSimilarity(b, epsilonArr));
-  List<int> co2scrub = numbers[0];
-  print('$o2gen, $co2scrub');
+  String o2genStr = filterByIndex(numbers, 0, '1', 'gt');
+  String co2scrubStr = filterByIndex(numbers, 0, '0', 'lt');
+
+  print(int.parse(o2genStr, radix: 2) * int.parse(co2scrubStr, radix: 2));
 }
 
 void main() async {
