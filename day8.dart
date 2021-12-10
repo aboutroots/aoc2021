7be06f45bd58950d4ceccc735bc4bb2dcf728faf
aboutroots <popiel.mw@gmail.com>
2021-12-10 20:34:05 +0100

    Day8 part2

--- day8.dart
+++ day8.dart
@@ -6,41 +6,55 @@ String sortString(String s) {
   return l.join('');
 }
 
+String diffStrings(String a, String b) {
+  Set charsA = new Set.from(a.split(''));
+  Set charsB = new Set.from(b.split(''));
+  return charsA.difference(charsB).join('');
+}
+
+List<int> toDigits(Entry e) {
+  List<String> patterns = e.patterns;
+
+  String one = patterns.firstWhere((p) => p.length == 2);
+  String four = patterns.firstWhere((p) => p.length == 4);
+  String seven = patterns.firstWhere((p) => p.length == 3);
+  String eight = patterns.firstWhere((p) => p.length == 7);
+  String nine = patterns.firstWhere((p) =>
+      p.length == 6 &&
+      diffStrings(seven, p) == '' &&
+      diffStrings(four, p) == '');
+  String zero = patterns.firstWhere(
+      (p) => p.length == 6 && diffStrings(seven, p) == '' && p != nine);
+  String six =
+      patterns.firstWhere((p) => p.length == 6 && p != nine && p != zero);
+  String three =
+      patterns.firstWhere((p) => p.length == 5 && diffStrings(one, p) == '');
+  String five =
+      patterns.firstWhere((p) => p.length == 5 && diffStrings(p, six) == '');
+  String two =
+      patterns.firstWhere((p) => p.length == 5 && p != five && p != three);
+
+  Map<String, int> digitMap = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+  };
+
+  return e.output.map((p) => digitMap[p]).toList();
+}
+
 class Entry {
   List<String> patterns;
   List<String> output;
 
-  @override
-  String toString() {
-    return '$this.patterns, $this.output';
-  }
-
   Entry(this.patterns, this.output);
-
-  List<int> parseOutputToDigits() {
-    List<int> digits = List.filled(4, -1);
-    return digits;
-
-    // while (digits.any((d) => d == -1)) {
-    //   this.output.forEach((letter) { })
-
-    // }
-    //   return this.output.map((s) {
-    //     if (s.length == 2) {
-    //       return 1;
-    //     }
-    //     if (s.length == 4) {
-    //       return 4;
-    //     }
-    //     if (s.length == 3) {
-    //       return 7;
-    //     }
-    //     if (s.length == 7) {
-    //       return 8;
-    //     }
-    //     return -1;
-    //   }).toList();
-  }
 }
 
 int solve(List<String> lines) {
@@ -51,22 +65,13 @@ int solve(List<String> lines) {
     return Entry(patterns, output);
   }).toList();
 
-  entries.forEach((e) {
-    print(e);
+  return entries.fold(0, (int count, Entry e) {
+    return count + int.parse(toDigits(e).join(''));
   });
-
-  // first
-  // return entries.fold(0, (int counter, element) {
-  //   return element
-  //           .parseOutputToDigits()
-  //           .fold(0, (int acc, int val) => acc + (val == -1 ? 0 : 1)) +
-  //       counter;
-  // });
-  // second
 }
 
 void main() async {
-  List<String> lines = await readlines('day8test.txt');
+  List<String> lines = await readlines('day8.txt');
   int result = solve(lines);
   print(result);
 }
